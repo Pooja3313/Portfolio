3a725b0884af26a8c8390aad9a025dfc9f2cc929
Pooja3313 <21cp324@bvmengineering.ac.in>
2023-07-05 17:39:46 +0530

    resume 4

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,48 @@
 import React from "react";
 import "./Navbar.css";
 import { NavLink } from "react-router-dom";
+import "../js/main"
+import { useEffect } from "react";
+import "jquery-ui-dist/jquery-ui";
+import $ from "jquery";
+var scrollto = require('scroll-to');
 
 
 function Navbar() {
+
+  // $(document).on('click', '.mobile-nav-toggle', function(e) {
+  //   $('#navbar').toggleClass('navbar-mobile');
+  //   $(this).toggleClass('bi-list bi-x');
+  // });
+  
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  // $(document).on('click', '.navbar .dropdown > a', function(e) {
+  //   if ($('#navbar').hasClass('navbar-mobile')) {
+  //     e.preventDefault();
+  //     $(this).next().toggleClass('dropdown-active');
+  //   }
+  // });
+  
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  // $(document).on('click', '.scrollto', function(e) {
+  //   if ($(this.hash).length) {
+  //     e.preventDefault();
+  
+  //     let navbar = $('#navbar');
+  //     if (navbar.hasClass('navbar-mobile')) {
+  //       navbar.removeClass('navbar-mobile');
+  //       let navbarToggle = $('.mobile-nav-toggle');
+  //       navbarToggle.toggleClass('bi-list bi-x');
+  //     }
+  //     scrollto(this.hash);
+  //   }
+  // });
+  
 	return(
 <>
  {/* <!-- ======= Header ======= --> */}
