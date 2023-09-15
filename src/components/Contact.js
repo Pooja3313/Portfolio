e1d28702f789fbd5499814f773feac0f8488547d
Pooja3313 <21cp324@bvmengineering.ac.in>
2023-09-15 11:17:49 +0530

    map

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -78,6 +78,12 @@ return (
         </div>
 
       </div>
+      <div className="container">
+        <div className="row">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29752.924978618674!2d72.87643422983417!3d21.227263204479996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be04f61ab9b35c7%3A0x7ccc9025485b7120!2sNana%20Varachha%2C%20Surat%2C%20Gujarat!5e0!3m2!1sen!2sin!4v1694753958418!5m2!1sen!2sin" width="600" height="450" style={{"border":"0"}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+        </div>
+      </div>
     </section>
     {/* <!-- End Contact Me Section --> */}
 
